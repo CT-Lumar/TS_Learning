907366075c592eab07305e954cfdc207330307e5
root <root@yakuza-back.us-west1-b.c.eng-nuance-417215.internal>
2024-03-22 19:50:01 +0000

    1

--- test.js
+++ test.js
@@ -4,3 +4,16 @@ numbers.push(4); // no error
 // numbers.push("2"); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
 // let head: number = numbers[0]; // no error
 console.log(numbers);
+// 如果沒過值，預設會給第一個 numeric enum as 0, 其他會自動累加
+// 所以如果自己給第一個設為 1，同理，從1開始累加
+var CardinalDirections;
+(function (CardinalDirections) {
+    CardinalDirections[CardinalDirections["North"] = 1] = "North";
+    CardinalDirections[CardinalDirections["Eats"] = 2] = "Eats";
+    CardinalDirections[CardinalDirections["South"] = 3] = "South";
+    CardinalDirections[CardinalDirections["West"] = 4] = "West";
+})(CardinalDirections || (CardinalDirections = {}));
+var currentDirections = CardinalDirections.North;
+console.log(currentDirections);
+var currentDirections2 = CardinalDirections.West;
+console.log(currentDirections2);
--- test.ts
+++ test.ts
@@ -4,3 +4,17 @@ numbers.push(4); // no error
 // numbers.push("2"); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
 // let head: number = numbers[0]; // no error
 console.log(numbers);
+
+
+// 如果沒過值，預設會給第一個 numeric enum as 0, 其他會自動累加
+// 所以如果自己給第一個設為 1，同理，從1開始累加
+enum CardinalDirections {
+	North = 1,
+	Eats,
+	South,
+	West
+}
+let currentDirections = CardinalDirections.North;
+console.log(currentDirections);
+let currentDirections2 = CardinalDirections.West;
+console.log(currentDirections2)
